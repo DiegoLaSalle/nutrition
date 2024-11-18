778805dd0d679a7224aed415cbc8bd27203f51a3
DiegoLaSalle <diego.arias@lasallistas.org.mx>
2024-11-17 21:22:04 -0600

    Add files via upload

--- nutrition_Library/wsgi.py
+++ nutrition_Library/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nutrition_Library.settings')
+
+settings_module = 'nutrition_Library.production' if 'WEBSITE_HOSTNAME' in os.environ else 'nutrition_Library.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
